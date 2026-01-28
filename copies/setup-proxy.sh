874e47995e987c35b5e2f3a569fffbfd5061b7bc
Andrew <acyuan7@gmail.com>
2026-01-27 17:30:51 -0800

    port

--- copies/setup-proxy.sh
+++ copies/setup-proxy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Set proxy (runs as root via LaunchDaemon)
-networksetup -setwebproxy "Wi-Fi" 127.0.0.1 8080
-networksetup -setsecurewebproxy "Wi-Fi" 127.0.0.1 8080
+networksetup -setwebproxy "Wi-Fi" 127.0.0.1 3594
+networksetup -setsecurewebproxy "Wi-Fi" 127.0.0.1 3594
 
 # This was a bad name -- we are using mitmdump, not mitmproxy
 su - acyuan -c "/Users/acyuan/Documents/Coding/filtererer/run_mitmproxy.sh"
